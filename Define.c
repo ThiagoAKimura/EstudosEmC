ce082b983ab01a853e5978600a5b9d26367fc267
ThiagoAKimura <thiagoayreskimura@gmail.com>
2022-05-17 11:17:53 -0300

    Define

---
+++ Define.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 10
+
+void main(){
+
+    //Imprimindo o define na tela
+    printf("%d\n", TAM);
+
+    //Contagem até 10 (for)
+
+    int i;
+
+    for(i=1; i <= 10; i++){
+
+        printf("%d\n",i);
+
+    }
+
+    //Pausa a execucao do programa
+    system("pause");
+
+}
